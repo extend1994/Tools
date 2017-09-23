1bd5f2af120e9cd6efea9cd71bb58c8d1b0e1d83
LboAnn <extend1994@gmail.com>
2017-09-23 16:55:46 +0800

    Add cdnjs working tools for the first time

---
+++ versionCompare.sh
@@ -0,0 +1,41 @@
+# parameters
+# @git_clone_url @npm_name
+# tmp1 is for git
+# tmp2 is for npm
+
+git clone $1
+git_dir=$(echo $1 | awk '{split($1, Arr, /[/.]/); print Arr[length(Arr)-1]}')
+echo "======================================"
+echo $git_dir
+cd $git_dir
+git tag > tmp1.txt
+mv tmp1.txt ../
+cd ../
+
+# Whether npm package name is the same as git repo
+if [ $# -eq 2 ]; then
+  npm show $2 versions > tmp2.txt
+fi
+
+# count lines of the result of npm show
+lines=$(wc -l tmp2.txt | awk '{print $1}')
+# Elimate spare symbols and let the result be the same as $(git tag)
+sed -i "s/v//" tmp1.txt
+sed -i "s/\[//" tmp2.txt
+sed -i "s/\]//" tmp2.txt
+
+if [ $lines != 1 ]; then
+  sed -i "s/,//" tmp2.txt
+else
+  sed -i "s/,/\n/" tmp2.txt
+fi
+
+sed -i "s/'//" tmp2.txt
+sed -i "s/'//" tmp2.txt
+sed -i "s/\ //" tmp2.txt
+sed -i "s/\ //" tmp2.txt
+
+# Go compare
+vimdiff tmp1.txt tmp2.txt
+# Remove all temporary files and folders.
+rm $git_dir tmp1.txt tmp2.txt -rf
---
+++ view.sh
@@ -0,0 +1,30 @@
+# parameters
+# @prNum @branch/folder name @contributor
+branchExist=$(git br| grep $2)
+if [ "$branchExist" != "" ] && [ $(git rev-parse --abbrev-ref HEAD) != $2 ]; then
+  echo -e "\033[33mBranch $2 exists! Now ready to checkout...\033[0m"
+  git co $2
+  echo -e "\033[32mCheckout to the branch $2 and update to latest status\033[0m"
+  git pull git@github.com:$3/cdnjs.git  $2:$2 --rebase -f
+else
+  git fetchpr $1
+  git co $1
+  git br -m $2
+fi
+
+libCheckout=$(grep $2 .git/info/sparse-checkout)
+if [ "$libCheckout" == "" ]; then
+  echo "/ajax/libs/$2/*" >> .git/info/sparse-checkout
+fi
+git rsh
+#libAbsolutePath=$(readlink ~/repos/cdnjs/ajax/libs/$2/ -nf)
+./tools/fixFormat.js
+cat ajax/libs/$2/package.json | rm ajax/libs/$2/$(jq .version -r) -rf
+cat ajax/libs/$2/package.json | echo -e "\033[33majax/libs/$2/$(jq .version -r) is removed\033[0m"
+./auto-update.js run $2
+# clean spare files
+tree ajax/libs/$2/
+git clean -df
+cat ajax/libs/$2/package.json | git s ajax/libs/$2/$(jq .version -r)
+git s
+echo -e "\033[32mdone!\033[0m"
