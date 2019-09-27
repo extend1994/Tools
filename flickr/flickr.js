1ec206c0edd7486a8cef99b98c0e3a6ce934aba5
LboAnn <extend1994@gmail.com>
2019-09-27 15:12:44 +0800

    Add helpful comments in flickr scripts

--- flickr/flickr.js
+++ flickr/flickr.js
@@ -2,6 +2,11 @@
 
 const url = require('flickr-photo-url');
 
+/*
+process.argv[0] is `node` command with its path
+process.argv[1] is flickr.js (called script's name)
+process.argv[2] would be photo's id which is used to find url on flickr
+*/
 url('99605377@N04', parseInt(process.argv[2]), url.sizes.large)
 .then(console.log)
 .catch(console.error)
--- flickr/flickr_url_converter.sh
+++ flickr/flickr_url_converter.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 
-#url=$(node flickr.js)
-#echo "echo url!"
-#echo $url
 READFILE=$1
+
+if [ $# -eq 0 ]; then
+  echo "Usage: ./<path_to>/flickr_url_converter.sh <READFILE>"
+  flag=0
+fi
 
 echo -n "" > replace.sh
 
-lines=$(wc $1 | awk '{print $1}')
+lines=$(wc $READFILE | awk '{print $1}')
 counter=0
 while read line; do
   ((counter+=1))
   photo_id=$(echo $line | awk -F '/' '{print $6}')
+  # Get gerneral urls using photo id from flickr
   url=$(node flickr.js $photo_id)
   now=$(echo "$counter $lines" | awk '{printf "%.3f", $1*100/$2}')
+  # Show how much work has been done
   echo -ne "$now%\r"
+  # Generate corresponding `sed` command to replace.sh in order to convert url in .md
+  # e.g. `sed 's///g'` *.md
   echo -n "sed 's/" >> replace.sh
   echo -n $line | sed 's/\//\\\//g' >> replace.sh
   echo -n "/" >> replace.sh
